0619352913afd0bd24b4c119fc1ab104f4ef6b68
shootface <31258676+shootface@users.noreply.github.com>
2019-10-23 22:40:55 -0500

    nuevas dependencias

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 3000,
+    db: process.env.DATABASE_URL || 'postgres://vislvvzieyztkh:8ff6283e69e09c2f8a3ce9756a0f17a98c977b06bc347639d9eb56f8f15a54a8@ec2-54-243-238-226.compute-1.amazonaws.com:5432/dgrs5s3ml954l'
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ const morgan = require('morgan');
 const app = express();
 
 //settings
-app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 //starting the serve
 app.listen(app.get('port'), () => {
     console.log('server on port ',app.get('port'));
 })
+
+const con = 'postgres://vislvvzieyztkh:8ff6283e69e09c2f8a3ce9756a0f17a98c977b06bc347639d9eb56f8f15a54a8@ec2-54-243-238-226.compute-1.amazonaws.com:5432/dgrs5s3ml954l';
