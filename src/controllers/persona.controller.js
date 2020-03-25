38b1ed6bce5a4e6f354120964dfe11b5f850f067
shootface <31258676+shootface@users.noreply.github.com>
2020-03-25 15:24:16 -0500

    update persona

--- src/controllers/persona.controller.js
+++ src/controllers/persona.controller.js
@@ -17,7 +17,26 @@ async function getPersonas(req,res){
         });
     }
 };
-
+async function getOnePersona(req,res){
+    const { id } = req.params;
+    try {
+        const pe = await Persona.persona.findOne({
+            where: {
+                pk_numero_identificacion: id
+            }
+        });
+        res.json({
+            data: pe
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Something goes wrong in getOneEmpleado',
+            data: error
+        });
+    }
+};
 //async function 
 
 module.exports.getPersonas = getPersonas;
+module.exports.getOnePersona = getOnePersona;
--- src/routes/personas.js
+++ src/routes/personas.js
@@ -3,5 +3,6 @@ const router = Router();
 const pController = require('../controllers/persona.controller');
 
 router.get('/',pController.getPersonas);
+router.get('/:id',pController.getOnePersona);
 
 module.exports = router;
