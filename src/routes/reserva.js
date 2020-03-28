60852451df5088cb57a068cf6e63e6088cf2b876
shootface <31258676+shootface@users.noreply.github.com>
2020-03-27 20:23:37 -0500

    update login

--- src/routes/reserva.js
+++ src/routes/reserva.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post('/',mVerified.auth,rCotroller.crear_reserva);
 router.post('/silla',mVerified.auth,rCotroller.reservar_silla);
 router.post('/snack',mVerified.auth,rCotroller.reservar_snack)
-router.get('/',rCotroller.disponibilidadSillas); // Se elimina la autenticación para pruebas con Capataz 14 de marzo de 2020
+router.get('/',mVerified.auth,rCotroller.disponibilidadSillas); // Se elimina la autenticación para pruebas con Capataz 14 de marzo de 2020
 
 module.exports = router;
